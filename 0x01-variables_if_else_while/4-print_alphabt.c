a4241f55a25b4fc805f56c381e22d2808306fb50
sewunetwolde <nekodmos@gmail.com>
2022-04-08 15:05:41 +0300

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Dscription: print lower case alphabet excluding e and q
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+for(c= 'a';c <='z'; ++c)
+{}
+if (c != 'e' && c != 'q')
+putchar(c);
+}
+putchar(c);
+
+return (0);
+}
