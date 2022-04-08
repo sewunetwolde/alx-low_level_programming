99092858495806d085be712bbf3c961a381b37e6
sewunetwolde <nekodmos@gmail.com>
2022-04-08 14:13:18 +0300

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print lower and upperalphabet no the line
+ * Return: o
+ */
+int main(void)
+{
+int c
+
+fot (c =0; c < 26; ++c)
+putchar('a' + c);
+for (c = 0; c < 26; ++c)
+putchar('A' + c);
+
+putchar('\n');
+
+return (0);
+}
