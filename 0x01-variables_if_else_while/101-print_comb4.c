baa0b9705e736a1d336e31006e517fdd3c1e08fd
sewunetwolde <nekodmos@gmail.com>
2022-04-11 16:58:24 +0300

    task12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (success/correct)
  */
-int main void()
+int main (void)
 {
 int dig1, dig2, dig3;
 for (dig1 = 0; dig1 < 8; dig1++)
@@ -13,7 +13,7 @@ for (dig2 = dig1 + 1; dig2 < 9; dig2++)
 {
 for (dig3 = dig2 + 1; dig3 < 10; dig3++)
 {
-putchar((did1 % 10) + '0');
+putchar((dig1 % 10) + '0');
 putchar((dig2 % 10) + '0');
 putchar((dig3 % 10) + '0');
 if (dig1 == 7 && dig2 == 8 dig3 == 9)
