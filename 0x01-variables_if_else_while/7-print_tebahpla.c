88390756445c7b514634523745eeaf333cf55958
sewunetwolde <nekodmos@gmail.com>
2022-04-11 15:08:07 +0300

    task8

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,12 +10,12 @@ int main(void)
 {
 char alphabet;
 
-for (alphabet = 122; alphabet <= 57; alphabet--)
+for (alphabet = 122; alphabet <= 97; alphabet--)
 {
-putchar (alphabet);
+putchar(alphabet);
 }
 
-putchar ('\n');
+putchat('\n');
 
 return (0); 
 
