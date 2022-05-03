506f973645ad94a3acb0ed80585eba3b9e534302
sewunetwolde <nekodmos@gmail.com>
2022-05-03 10:54:07 +0300

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,79 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-char **spll(char *str, int cont1);
-/**
-  * strtow - splits a string into words
-  * @str: a string with spaces
-  * Return: an address
-  */
-
-char **strtow(char *str)
-{
-	int i, cont1 = 0, flag1 = 0;
-
-	if (str == NULL || *str == '\0')
-		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] != ' ' && flag1 == 0)
-		{
-			cont1++;
-			flag1 = 1;
-		}
-		else if (flag1 == 1 && str[i + 1] == ' ')
-			flag1 = 0;
-	}
-	if (cont1 == 0)
-		return (NULL);
-
-	return (spll(str, cont1));
-}
 
 /**
-  * spll - splits a string into words
-  * @str: a string with spaces
-  * @cont1: is a counter
-  * Return: an address
-  */
-
-char **spll(char *str, int cont1)
+ * argstostr - Concatenates all arguments of the program into a string;
+ *             arguments are separated by a new line in the string.
+ * @ac: The number of arguments passed to the program.
+ * @av: An array of pointers to the arguments.
+ *
+ * Return: If ac == 0, av == NULL, or the function fails - NULL.
+ *         Otherwise - a pointer to the new string.
+ */
+char *argstostr(int ac, char **av)
 {
-	int i, j, k, cont2, flag1 = 0;
-	char **splitt;
+	char *str;
+	int arg, byte, index, size = ac;
 
-	splitt = malloc((cont1 + 1) * sizeof(char *));
-	if (!splitt)
+	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (i = 0, j = 0, flag1 = 0, cont2 = 0; i < cont1; i++, j++)
+
+	for (arg = 0; arg < ac; arg++)
 	{
-		for ( ; str[j] != '\0'; j++)
-		{
-			if (str[j] != ' ')
-			{
-				cont2++;
-				flag1 = 1;
-			}
-			if (flag1 == 1 && (str[j + 1] == ' ' || str[j + 1] == '\0'))
-			{
-				flag1 = 0;
-				splitt[i] = malloc(cont2 * sizeof(char) + 1);
-				if (splitt[i] == NULL)
-				{
-					for (i--; i >= 0; i--)
-						free(splitt[i]);
-					free(splitt);
-					return (NULL);
-				}
-				for (k = 0; k < cont2; k++)
-					splitt[i][k] = str[(j - cont2 + 1) + k];
-				splitt[i][k] = '\0';
-				cont2 = 0;
-				break;
-			}
-		}
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
 	}
-	splitt[i] = '\0';
-	return (splitt);
+
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	index = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+
+		str[index++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
 }
